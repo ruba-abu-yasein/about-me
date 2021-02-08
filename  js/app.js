dd1c77e8f48f8e4227d37c3c20f17f2fd777a42a
ruba-abu-yasein <abuyaseinruba@gmail.com>
2021-02-08 17:26:10 +0200

    monday

---
+++  js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+let job = prompt('am i doctor?');
+if(job.toLowerCase()==='yes'){
+    alert('no way!');
+}else if(job ==='no'){
+    alert('good one')
+    console.log('correct answer');
+}
+let gender = prompt('am i male?');
+if(gender.toLowerCase()==='yes'){
+    alert('what!!!');
+}else if(gender==='no'){
+    alert('correct');
+    console.log('correct answer');
+}
+let place = prompt('do i live in space?');
+if (place==='yes'){
+    alert('hi alien');
+}else if(place==='no'){
+    alert('of course');
+    console.log('correct answer');
+}
+let age = prompt('am i grand mother?');
+if (age==='yes'){
+    alert('realy!!!');
+}else if(age==='no'){
+    alert('that\'s right');
+    console.log('correct answer');
+}
+let point = prompt('Do i will git 10 points?');
+if (point==='yes'){
+    alert('Yappy!');
+    console.log('correct answer');
+}else if(point==='no'){
+    alert('whyyyy!');
+}
